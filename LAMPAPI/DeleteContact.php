81f26fec950a846d46f0debf4c255c7ad40fc295
Leonardo Faife <120056923+lfaife@users.noreply.github.com>
2025-10-06 22:10:20 -0400

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
     $inData = getRequestInfo();
     // Get incoming JSON data from request
@@ -46,4 +47,54 @@
         sendResultInfoAsJson( $retValue );
     }
 	
+=======
+<?php
+    $inData = getRequestInfo();
+    // Get incoming JSON data from request
+    $conn = new mysqli("143.110.151.237", "POOS_db", "Small_2025_Project", "poos_app");
+    if ($conn->connect_error) 
+    {
+        returnWithError($conn->connect_error);
+    } 
+    else
+    {
+        // Delete contact for specified contact ID and user ID
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID=? AND UserID=?");
+        $stmt->bind_param("ii", $inData["contactId"], $inData["userId"]);
+        $stmt->execute();
+        // Check if deletion was successful
+        if($stmt->affected_rows > 0)
+        {
+            returnWithInfo("Contact deleted successfully");
+        }
+        else
+        {
+            returnWithError("Contact not found");
+        }
+    }
+    // Decode JSON from request body
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+    // Send JSON response
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    // Return error response
+    function returnWithError( $err )
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+    // Return success response
+    function returnWithInfo( $message )
+    {
+        $retValue = '{"message":"' . $message . '","error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
+	
+>>>>>>> 5b9c54237ff4a3ce8a710b6fe4771093f8a47c55
 ?>
